d71eb8ed729da67d1579b82c7fec1aeb6c8e4eb5
Nexusrex18 <lavisnj350@gmail.com>
2025-02-12 21:42:03 +0530

    backend rendering fix

--- backend/server.go
+++ backend/server.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"time"
+	"os"
 
 	"lumine/backend/routes"
 
@@ -19,20 +20,20 @@ func main() {
 	routes.RegisterRoutes(r)
 
 	// Enable CORS to allow requests from the frontend (e.g., http://localhost:3000)
-	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
+	allowedOrigins := handlers.AllowedOrigins([]string{os.Getenv("CORS_ALLOWED_ORIGINS")})
 	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"})
 	allowedHeaders := handlers.AllowedHeaders([]string{"Content-Type", "Authorization"})
 
 	// Set up a server with custom configurations
 	server := &http.Server{
 		Handler:      handlers.CORS(allowedOrigins, allowedMethods, allowedHeaders)(r), // Apply CORS middleware
-		Addr:         ":8080",                                                          // Server listens on port 8080
+		Addr:         ":" + os.Getenv("PORT"),                                                          // Server listens on port 8080
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 		IdleTimeout:  60 * time.Second,
 	}
 
-	log.Println("Server is running on http://localhost:8080")
+	log.Println("Server is running on port", os.Getenv("PORT"))
 	// Start the server
 	log.Fatal(server.ListenAndServe())
 }
